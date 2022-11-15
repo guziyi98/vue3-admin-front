b91a64d5a8e0b176d17407501fd2e55836360ee5
guziyi <guziyi@plantdata.cn>
2022-11-15 22:46:07 +0800

    feat: del husky

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -27,7 +27,7 @@ module.exports = {
       "error",
       {
         singleQuote: false, // 使用单引号
-        semi: true, // 末尾不添加分号
+        semi: false, // 末尾不添加分号
         tabWidth: 2, // 2个间距
         trailingComma: "none",
         useTabs: false,
@@ -35,4 +35,4 @@ module.exports = {
       }
     ]
   }
-};
+}
--- .prettierrc.js
+++ .prettierrc.js
@@ -1,6 +1,6 @@
 module.exports = {
   singleQuote: false, // 使用单引号
-  semi: true, // 末尾不添加分号
+  semi: false, // 末尾不添加分号
   tabWidth: 2, // 2个间距
   trailingComma: "none",
   useTabs: false,
